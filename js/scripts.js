775dc01dfc19b01366fb36f6c560fcddfeedaf22
DAVID MARTINDALE <dlmartindale99@gmail.com>
2023-02-16 20:46:12 -0800

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,127 @@
+/*
+    App: Assigment 1 - COMP 690
+    Author: David Martindale
+    Date: 02/14/2023
+*/
+
+//STEP 1
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+//STEP 2
+3.14
+"hello there!"
+true
+null
+
+//STEP 3
+let anExpression = 9 * (4/5) + 6
+let anotherExpression = function (x) {return x*x}
+
+//STEP 4
+let firstName
+let lastName
+let strAddress
+let strCity
+let strState
+let strZip
+let yourAge
+let referralSource
+let mayWeCotactYou
+
+//STEP 5
+firstName = "David"
+strState = "California"
+yourAge = 23
+
+//STEP 6
+var myVar = 42
+var strResult = `${myVar} is the answer.`
+console.log(strResult)
+
+//STEP 7
+var strOne = true + " is a truthy value."
+console.log(strOne)
+
+var strTwo = 5 + true
+console.log(strTwo)
+
+//STEP 8
+let strAnswer = "Yes, the string literal is valid. It uses single quotes to delimit the string, which is useful when the string contains double quotes so that we dont have to escape them."
+
+//STEP 9
+let x2 = null
+let five
+
+//STEP 10
+console.log(typeof "hello")
+console.log(typeof 42)
+console.log(typeof true)
+console.log(typeof {name: "David"})
+console.log(typeof undefined)
+
+//STEP 11
+alert("Hello David" + ", welcome to the JavaScript class!") 
+
+//STEP 12
+let strName = "David"
+alert("Hello " + strName + ", welcome to the Javascript class!")
+
+//STEP 13
+let strCourse = "JavaScript"
+alert("Hello " + strName + ", welcome to the " + strCourse + " class!")
+
+//STEP 14
+alert("Hello " + strName + ", \nWelcome to the " + strCourse + " class!")
+
+//STEP 15
+let userName = prompt("What is your name?")
+
+//STEP 16
+let userCourse = prompt("What course are you taking?")
+
+//STEP 17
+x = 10
+y = 20
+console.log(x + y)
+
+//STEP 18
+x = 20
+x += 20
+
+console.log(x)
+
+//STEP 19
+x = 20
+x *= 5
+console.log(x)
+
+//STEP 20
+x = 20 
+x = x % 3
+console.log(x)
+
+//STEP 21
+x = 10
+y = 20
+z = 30
+
+if (x < y && y < z) {
+    console.log("The expression is true!")
+} else {
+    console.log("The expression is false.")
+}
+
+//STEP 22
+x = 10
+y = 20
+z = 30
+
+if (x > y && y > z) {
+    console.log("The expression is true!")
+} else {
+    console.log("The expression is false.")
+}
